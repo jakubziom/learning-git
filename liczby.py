a00f566695302eeb961a4d14a50f524d608a1c3e
Jakub Ziomek <ziomek.jakub@wp.pl>
2024-05-22 11:47:16 +0200

    added a loop

--- liczby.py
+++ liczby.py
@@ -1,3 +1,8 @@
 numbers_divided_by5=[]
 numbers_divided_by5_power_of_3=[]
 
+for i in range(0,100):
+    if i%5 ==0:
+        numbers_divided_by5.append(i)
+        numbers_divided_by5_power_of_3.append(i**3)
+
