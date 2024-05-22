efce03cbde23e482859b5a25f3aa24cc9cd477f1
Jakub Ziomek <ziomek.jakub@wp.pl>
2024-05-22 11:47:53 +0200

    added a print function

--- liczby.py
+++ liczby.py
@@ -6,3 +6,5 @@ for i in range(0,100):
         numbers_divided_by5.append(i)
         numbers_divided_by5_power_of_3.append(i**3)
 
+print(numbers_divided_by5)
+print(numbers_divided_by5_power_of_3)
