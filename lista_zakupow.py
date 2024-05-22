e37403490d7551aa58160fced30c29bc88c9322b
Jakub Ziomek <ziomek.jakub@wp.pl>
2024-05-22 08:33:27 +0200

    Finished Task

--- lista_zakupow.py
+++ lista_zakupow.py
@@ -1,3 +1,9 @@
 print("Lista Zakupów")
 shopping_dict={'piekarnia':['chleb','pączek','bułki'],'warzywniak':['marchew','seler','rukola']}
 shopping_items_count=[]
+
+for item in shopping_dict:
+    print('Idę do ' + str(item).capitalize() + ', kupuję tu następujące rzeczy: ' + str(shopping_dict[item]).title())
+    shopping_items_count.append((len(shopping_dict[item])))
+ 
+print('W sumie kupuję ' + str(sum(shopping_items_count)) + ' produktów.')
